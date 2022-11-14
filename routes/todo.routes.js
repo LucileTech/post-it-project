1b1d38bd24c9682c3546561d12ee8bc5d0a3d28e
Amélie Chabrand <amelie.chabrand@gmail.com>
2022-11-14 12:41:38 +0100

    routes in process

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -2,36 +2,65 @@ const express = require("express");
 const router = express.Router();
 const Todo = require("../models/Todo.model");
 
-/* POST todo page */
-
-router.post("/todo/newtodo", async (req, res, next) => {
-  try {
-    const { title, content } = req.body;
-    await Todo.create({ title, content });
-    // res.redirect("/todo/");
-  } catch (error) {
-    next(error);
-  }
-});
-
 router.get("/todos", async (req, res, next) => {
   try {
     const allTodos = await Todo.find();
     console.log(allTodos);
-    res.render("todo/alltodoview", { allTodos });
+    res.render("todo/alltodoview", {
+      allTodos,
+      style: ["todostyle.css"],
+    });
   } catch (error) {
     next(error);
   }
 });
 
-router.get("/todo/:id", async (req, res, next) => {
+/* POST todo page */
+
+router.get("/todos/create", (req, res, next) => {
   try {
-    const myTodo = await Todo.findById(req.params.id);
+    res.render("todo/createtodoview");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/todos/create", async (req, res, next) => {
+  try {
+    const { title, content } = req.body;
+    await Todo.create({ title, content });
+    res.redirect("/todos");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/todos/:id", async (req, res, next) => {
+  try {
+    const oneTodo = await Todo.findById(req.params.id);
     console.log(req.params.id);
-    res.render("todo/todoview", {
-      myTodo,
+    res.render("todo/onetodoview", {
+      oneTodo,
       style: ["todostyle.css"],
     });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Update route to do entière
+router.post("/todos/:id", (res, req, next) => {
+  try {
+    res.redirect("/todos");
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Delete route to do entière
+router.post("/todos/:id", (res, req, next) => {
+  try {
+    res.redirect("/todos");
   } catch (error) {
     next(error);
   }
